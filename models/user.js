7acef60aeee476de8c2755495a46951f2ae95eba
Katya Greimes <katyagreimes@gmail.com>
2024-08-14 19:33:00 -0700

    Passport

--- models/user.js
+++ models/user.js
@@ -1,26 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     },
     level: {
         type: Number,
+        default: 0
     },
     points: {
         type: Number,
+        default: 0
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
