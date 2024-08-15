78f356e7f37b6893d316019373e49a73960044bc
Katya Greimes <katyagreimes@gmail.com>
2024-08-15 09:24:18 -0700

    Week 3 workshop

--- routes/levelRouter.js
+++ routes/levelRouter.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const Level = require("../models/levelSchema");
 const levelRouter = express.Router();
-const authenticate = require('../authenticate');
+const authenticate = require("../authenticate");
 
-levelRouter.route('/')
-    .get((req, res, next) => {
-        Level.find()
-        .populate('feedbacks.author')
-        .then(levels => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(levels);
-        })
-        .catch(err => next(err));
-    })
-  .post(authenticate.verifyUser, (req, res, next) => {
+
+levelRouter
+  .route("/")
+  .get((req, res, next) => {
+    Level.find()
+      .populate("feedbacks.author")
+      .then((levels) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(levels);
+      })
+      .catch((err) => next(err));
+  })
+  .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Level.create(req.body)
       .then((level) => {
         console.log("level Created ", level);
@@ -42,19 +44,19 @@ levelRouter
   .route("/:levelId")
   .get((req, res, next) => {
     Level.findById(req.params.levelId)
-    .populate('feedbacks.author')
-    .then(level => {
+      .populate("feedbacks.author")
+      .then((level) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(level);
-    })
-    .catch(err => next(err));
-})
+      })
+      .catch((err) => next(err));
+  })
   .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /levels/${req.params.levelId}`);
   })
-  .put(authenticate.verifyUser, (req, res, next) => {
+  .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Level.findByIdAndUpdate(
       req.params.levelId,
       {
@@ -79,147 +81,158 @@ levelRouter
       .catch((err) => next(err));
   });
 
-  levelRouter.route('/:levelId/feedbacks')
-.get((req, res, next) => {
+levelRouter
+  .route("/:levelId/feedbacks")
+  .get((req, res, next) => {
     Level.findById(req.params.levelId)
-    .populate('feedbacks.author')
-        .then(level => {
-            if (level) {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(level.feedbacks);
-            } else {
-                err = new Error(`Level ${req.params.levelId} not found`);
-                err.status = 404;
-                return next(err);
-            }
-        })
-        .catch(err => next(err));
-    })
-.post(authenticate.verifyUser, (req, res, next) => {
-    Level.findById(req.params.levelId)
-    .then(level => {
+      .populate("feedbacks.author")
+      .then((level) => {
         if (level) {
-            level.feedbacks.push(req.body);
-            level.save()
-            .then(level => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(level);
-            })
-            .catch(err => next(err));
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(level.feedbacks);
         } else {
-            err = new Error(`Level ${req.params.levelId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`Level ${req.params.levelId} not found`);
+          err.status = 404;
+          return next(err);
         }
-    })
-    .catch(err => next(err));
-})
-.put(authenticate.verifyUser, (req, res) => {
+      })
+      .catch((err) => next(err));
+  })
+  .post(authenticate.verifyUser, (req, res, next) => {
+    Level.findById(req.params.levelId)
+      .then((level) => {
+        if (level) {
+          req.body.author = req.user._id;
+          level.feedbacks.push(req.body);
+          level
+            .save()
+            .then((level) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(level);
+            })
+            .catch((err) => next(err));
+        } else {
+          err = new Error(`Level ${req.params.levelId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  })
+  .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
-    res.end(`PUT operation not supported on /levels/${req.params.levelId}/feedbacks`);
-})
-.delete(authenticate.verifyUser, (req, res, next) => {
+    res.end(
+      `PUT operation not supported on /levels/${req.params.levelId}/feedbacks`
+    );
+  })
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Level.findById(req.params.levelId)
-    .then(level => {
+      .then((level) => {
         if (level) {
-            for (let i = (level.feedbacks.length-1); i >= 0; i--) {
-                level.feedbacks.id(level.feedbacks[i]._id).deleteOne();
-            }
-            level.save()
-            .then(level => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(level);
+          for (let i = level.feedbacks.length - 1; i >= 0; i--) {
+            level.feedbacks.id(level.feedbacks[i]._id).deleteOne();
+          }
+          level
+            .save()
+            .then((level) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(level);
             })
-            .catch(err => next(err));
+            .catch((err) => next(err));
         } else {
-            err = new Error(`Level ${req.params.levelId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`Level ${req.params.levelId} not found`);
+          err.status = 404;
+          return next(err);
         }
-    })
-    .catch(err => next(err));
-});
+      })
+      .catch((err) => next(err));
+  });
 
-levelRouter.route('/:levelId/feedbacks/:feedbackId')
-.get((req, res, next) => {
+levelRouter
+  .route("/:levelId/feedbacks/:feedbackId")
+  .get((req, res, next) => {
     Level.findById(req.params.levelId)
-    .populate('feedbacks.author')
-    .then(level => {
+      .populate("feedbacks.author")
+      .then((level) => {
         if (level && level.feedbacks.id(req.params.feedbackId)) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(level.feedbacks.id(req.params.feedbackId));
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(level.feedbacks.id(req.params.feedbackId));
         } else if (!level) {
-            err = new Error(`Level ${req.params.levelId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`Level ${req.params.levelId} not found`);
+          err.status = 404;
+          return next(err);
         } else {
-            err = new Error(`Feedback ${req.params.feedbackId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`Feedback ${req.params.feedbackId} not found`);
+          err.status = 404;
+          return next(err);
         }
-    })
-    .catch(err => next(err));
-})
-.post(authenticate.verifyUser, (req, res) => {
+      })
+      .catch((err) => next(err));
+  })
+  .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /levels/${req.params.levelId}/feedbacks/${req.params.feedbackId}`);
-})
-.put(authenticate.verifyUser, (req, res, next) => {
+    res.end(
+      `POST operation not supported on /levels/${req.params.levelId}/feedbacks/${req.params.feedbackId}`
+    );
+  })
+  .put(authenticate.verifyUser, (req, res, next) => {
     Level.findById(req.params.levelId)
-    .then(level => {
+      .then((level) => {
         if (level && level.feedbacks.id(req.params.feedbackId)) {
-            if (req.body.rating) {
-                level.feedbacks.id(req.params.feedbackId).rating = req.body.rating;
-            }
-            if (req.body.text) {
-                level.feedbacks.id(req.params.feedbackId).text = req.body.text;
-            }
-            level.save()
-            .then(level => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(level);
+          if (req.body.rating) {
+            level.feedbacks.id(req.params.feedbackId).rating = req.body.rating;
+          }
+          if (req.body.text) {
+            level.feedbacks.id(req.params.feedbackId).text = req.body.text;
+          }
+          level
+            .save()
+            .then((level) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(level);
             })
-            .catch(err => next(err));
+            .catch((err) => next(err));
         } else if (!level) {
-            err = new Error(`Level ${req.params.levelId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`Level ${req.params.levelId} not found`);
+          err.status = 404;
+          return next(err);
         } else {
-            err = new Error(`Feedback ${req.params.feedbackId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`Feedback ${req.params.feedbackId} not found`);
+          err.status = 404;
+          return next(err);
         }
-    })
-    .catch(err => next(err));
-})
-.delete(authenticate.verifyUser, (req, res, next) => {
+      })
+      .catch((err) => next(err));
+  })
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Level.findById(req.params.levelId)
-    .then(level => {
+      .then((level) => {
         if (level && level.feedbacks.id(req.params.feedbackId)) {
-            level.feedbacks.id(req.params.feedbackId).deleteOne();
-            level.save()
-            .then(level => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(level);
+          level.feedbacks.id(req.params.feedbackId).deleteOne();
+          level
+            .save()
+            .then((level) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "application/json");
+              res.json(level);
             })
-            .catch(err => next(err));
+            .catch((err) => next(err));
         } else if (!level) {
-            err = new Error(`Level ${req.params.levelId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`Level ${req.params.levelId} not found`);
+          err.status = 404;
+          return next(err);
         } else {
-            err = new Error(`feedback ${req.params.feedbackId} not found`);
-            err.status = 404;
-            return next(err);
+          err = new Error(`feedback ${req.params.feedbackId} not found`);
+          err.status = 404;
+          return next(err);
         }
-    })
-    .catch(err => next(err));
-});
+      })
+      .catch((err) => next(err));
+  });
 
 module.exports = levelRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,66 +1,107 @@
-const express = require('express');
-const User = require('../models/user');
-const passport = require('passport');
-const authenticate = require('../authenticate');
+const express = require("express");
+const User = require("../models/user");
+const passport = require("passport");
+const authenticate = require("../authenticate");
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+/* router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.post('/signup', (req, res) => {
-  User.register(
-      new User({username: req.body.username}),
-      req.body.password,
-      (err, user) => {
-          if (err) {
-              res.statusCode = 500;
-              res.setHeader('Content-Type', 'application/json');
-              res.json({err: err});
-          } else {
-              if (req.body.firstname) {
-                  user.firstname = req.body.firstname;
-              }
-              if (req.body.lastname) {
-                  user.lastname = req.body.lastname;
-              }
-              user.save(err => {
-                  if (err) {
-                      res.statusCode = 500;
-                      res.setHeader('Content-Type', 'application/json');
-                      res.json({err: err});
-                      return;
-                  }
-                  passport.authenticate('local')(req, res, () => {
-                      res.statusCode = 200;
-                      res.setHeader('Content-Type', 'application/json');
-                      res.json({success: true, status: 'Registration Successful!'});
-                  });
-              });
-          }
-      }
-  );
-});
+router.post("/signup", async (req, res) => {
+  try {
 
-router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
-  const token = authenticate.getToken({_id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
-});
+    const user = await new Promise((resolve, reject) => {
+      User.register(
+        new User({ username: req.body.username }),
+        req.body.password,
+        (err, user) => {
+          if (err) reject(err);
+          else resolve(user);
+        }
+      );
+    });
 
-router.get('/logout', (req, res, next) => {
-    if (req.session) {
-        req.session.destroy();
-        res.clearCookie('session-id');
-        res.redirect('/');
-    } else {
-        const err = new Error('You are not logged in!');
-        err.status = 401;
-        return next(err);
+    if (req.body.firstname) {
+      user.firstname = req.body.firstname;
     }
+    if (req.body.lastname) {
+      user.lastname = req.body.lastname;
+    }
+
+    await new Promise((resolve, reject) => {
+      user.save((err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+
+    passport.authenticate("local")(req, res, () => {
+      res
+        .status(200)
+        .json({ success: true, status: "Registration Successful!" });
+    });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+}); */
+
+router.post("/signup", async (req, res) => {
+  try {
+    // Register the user with username and password
+    const user = new User({ username: req.body.username });
+    const registeredUser = await User.register(user, req.body.password);
+
+    // Update additional fields if provided
+    if (req.body.firstname) {
+      registeredUser.firstname = req.body.firstname;
+    }
+    if (req.body.lastname) {
+      registeredUser.lastname = req.body.lastname;
+    }
+
+    // Save the user with the additional fields
+    await registeredUser.save();
+
+    // Authenticate the user
+    passport.authenticate("local")(req, res, () => {
+      res
+        .status(200)
+        .json({ success: true, status: "Registration Successful!" });
+    });
+  } catch (err) {
+    // Handle errors
+    res.status(500).json({ err });
+  }
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!",
+    });
+  }
+);
+
+router.get("/logout", (req, res, next) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    const err = new Error("You are not logged in!");
+    err.status = 401;
+    return next(err);
+  }
 });
 
 module.exports = router;
